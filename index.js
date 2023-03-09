ad058f3d8114ae408f51555920829dfb67d0573b
unknown <mielin@ucdavis.edu>
2023-03-08 21:32:28 -0800

    added slashes to js image src

--- index.js
+++ index.js
@@ -3,31 +3,31 @@ window.onload = () => {
     //BODY
     const body = new Image()
     const bodynum = Math.floor(Math.random()*12)+1
-    body.src = `assets/body/body-${bodynum}.png`
+    body.src = `/assets/body/body-${bodynum}.png`
 
     //EARS
     const ears = new Image()
     const earsnum = Math.floor(Math.random()*11)+1
-    ears.src = `assets/ears/ears-${earsnum}.png`
+    ears.src = `/assets/ears/ears-${earsnum}.png`
 
     //OUTLINE
     const outline = new Image()
-    outline.src = `assets/outline.png`
+    outline.src = `/assets/outline.png`
 
     //FACE
     const face = new Image()
     const facenum = Math.floor(Math.random()*8)+1
-    face.src = `assets/face/face-${facenum}.png`
+    face.src = `/assets/face/face-${facenum}.png`
 
     //ACCESSORY
     const accessory = new Image()
     const accessorynum = Math.floor(Math.random()*7)+1
-    accessory.src = `assets/accessory/accessory-${accessorynum}.png`
+    accessory.src = `/assets/accessory/accessory-${accessorynum}.png`
 
     //HAT
     const hat = new Image()
     const hatnum = Math.floor(Math.random()*20)+1
-    hat.src = `assets/hat/hat-${hatnum}.png`
+    hat.src = `/assets/hat/hat-${hatnum}.png`
 
     //BUILD
     body.onload = () => {
